f99d05e5c26551ce119480a17e2f5610e37884d0
Syifa Dwiky Basamala <119727680+syifadwikyb@users.noreply.github.com>
2024-11-28 13:33:11 +0700

    Update gradle

--- app/src/main/java/com/example/eatwise/activity/MainActivity.kt
+++ app/src/main/java/com/example/eatwise/activity/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.eatwise.activity
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
