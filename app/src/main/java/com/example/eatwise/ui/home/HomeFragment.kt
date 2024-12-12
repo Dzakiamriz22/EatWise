2fc39efb3a265adca502e53779d06c62adecb425
Dzakiamriz22 <dzakiamriz12@gmail.com>
2024-12-12 14:38:47 +0700

    mod : recomendation item

--- app/src/main/java/com/example/eatwise/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/eatwise/ui/home/HomeFragment.kt
@@ -1,4 +1,3 @@
-// Updated HomeFragment.kt
 package com.example.eatwise.ui.home
 
 import android.content.Context
@@ -7,7 +6,6 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -98,7 +96,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     }
 
     private fun getRecommendations(bmi: Float): List<String> {
-        return when {
+        val recommendations = when {
             bmi < 18.5 -> listOf(
                 "Carbohydrates: 300g",
                 "Proteins: 70g",
@@ -124,5 +122,14 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                 "Calories: 1500 kcal"
             )
         }
+
+        val dailyWater = when {
+            bmi < 18.5 -> "Water: 3.5 liters/day"
+            bmi in 18.5..24.9 -> "Water: 3 liters/day"
+            bmi in 25.0..29.9 -> "Water: 2.5 liters/day"
+            else -> "Water: 2 liters/day"
+        }
+
+        return recommendations + dailyWater
     }
 }
